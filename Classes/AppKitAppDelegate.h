2557df567894ab711f31ee251f46f2d0dcb0d310
Rupert de Guzman <rndguzmanjr@gmail.com>
2011-03-04 11:26:12 +1100

    Initial Import

---
+++ Classes/AppKitAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppKitAppDelegate.h
+//  AppKit
+//
+//  Created by rupert on 4/03/11.
+//  Copyright 2011 2RMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppKitAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AppKit
+//
+//  Created by rupert on 4/03/11.
+//  Copyright 2011 2RMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
